b1e4a45533c03c5143192ddca115b72897c630b5
= <zsolt@sowad.com>
2016-11-28 12:34:49 +0000

    Update URL in sample application

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@
   const output = document.getElementById('output');
 
   input.addEventListener('keyup', function(event) {
-    fetch('http://fcc-api-ts.herokuapp.com/' + encodeURIComponent(input.value))
+    fetch('/' + encodeURIComponent(input.value))
     .then(res => res.json())
     .then(res => output.value = JSON.stringify(res, null, 2))
     .catch(err => console.error(err));
